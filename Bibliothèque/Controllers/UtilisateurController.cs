25d8010bf09f6c089ed04d5f7bb73949e015ba00
Juliette Rivière <juliette@riviere.im>
2016-01-15 13:24:24 +0100

    User + liens entre pages

---
+++ Bibliothèque/Controllers/UtilisateurController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bibliothèque.Controllers
+{
+    public class UtilisateurController : Controller
+    {
+        // GET: Utilisateur
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
